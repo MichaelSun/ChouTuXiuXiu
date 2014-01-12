bfc04664de0b5c481b083da318bb60290a132d14
Shaofeng <wang123sf@renren-inc.com>
2014-01-12 23:49:48 +0800

    修复统计是图片无法上传的问题

--- src/com/canruoxingchen/uglypic/statistics/StatisticsUtil.java
+++ src/com/canruoxingchen/uglypic/statistics/StatisticsUtil.java
@@ -1,6 +1,8 @@
 package com.canruoxingchen.uglypic.statistics;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import android.content.Context;
 import android.telephony.TelephonyManager;
@@ -8,8 +10,10 @@ import android.telephony.TelephonyManager;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVFile;
 import com.avos.avoscloud.AVObject;
+import com.canruoxingchen.uglypic.concurrent.ThreadPoolManager;
 import com.canruoxingchen.uglypic.footage.FootAge;
 import com.canruoxingchen.uglypic.footage.NetSence;
+import com.canruoxingchen.uglypic.util.Logger;
 
 public class StatisticsUtil {
 	public static void increaseFootageCount(String objectId) {
@@ -26,20 +30,30 @@ public class StatisticsUtil {
 		avo.saveInBackground();
 	}
 
-	public static void saveFile(Context context, String path) {
+	public static void saveFile(Context context, final String path) {
 		TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		String imei = tm == null ? "" : tm.getDeviceId();
-		AVFile avFile;
-		try {
-			AVObject avObject = new AVObject("SocialObject");
-			File file = new File(path);
-			avFile = new AVFile(file.getName(), path);
-			avFile.save();
-			avObject.put("exNmae", imei);
-			avObject.put("exIcon", avFile);
-			avObject.saveInBackground();
-		} catch (AVException e) {
-			
-		}
+		final String imei = tm == null ? "" : tm.getDeviceId();
+		ThreadPoolManager.getInstance().execute(new Runnable() {
+
+			@Override
+			public void run() {
+				AVFile avFile;
+				try {
+					File file = new File(path);
+					avFile = AVFile.withAbsoluteLocalPath(file.getName(), path);
+					avFile.save();
+					AVObject avObject = new AVObject("SocialObject");
+					avObject.put("exNmae", imei);
+					avObject.put("exIcon", avFile);
+					avObject.saveInBackground();
+				} catch (AVException e) {
+					Logger.d(e == null ? "AVException" : e.getMessage());
+				} catch (FileNotFoundException e) {
+					Logger.d(e == null ? "FileNotFoundException" : e.getMessage());
+				} catch (IOException e) {
+					Logger.d(e == null ? "IOException" : e.getMessage());
+				}
+			}
+		});
 	}
 }
